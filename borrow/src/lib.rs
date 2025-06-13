52916e0847604a23c36a9fcc2111a4d0c38b2e4b
yahya houssaini <erensas007@gmail.com>
2025-06-13 16:01:22 +0100

    borrowed day two

---
+++ borrow/src/lib.rs
@@ -0,0 +1,20 @@
+pub fn str_len(s: &str) -> usize {
+    s.chars().count()
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+    let s = "hello";
+	let s1 = "camelCase".to_string();
+	let s2 = "olá!";
+
+	println!("\tstr_len(\"{}\") = {}", s, str_len(s));
+	println!("\tstr_len(\"{}\") = {}", s1, str_len(&s1));
+	println!("\tstr_len(\"{}\") = {}", s2, str_len(s2));
+    }
+}
