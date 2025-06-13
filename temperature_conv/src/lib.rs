81c9bdbc61b570054148138a25988637b9a44d9a
yahya houssaini <erensas007@gmail.com>
2025-06-13 14:09:46 +0100

    pres

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,5 +1,5 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    (f-32.0)*5.0/9.0
+    (f-32 as f64)/(9 as f64/5 as f64)
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
@@ -11,7 +11,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        println!("{} F = {} C", -459.67, fahrenheit_to_celsius(-459.67));
-            println!("{} C = {} F", 0.0, celsius_to_fahrenheit(0.0));
+        println!("{} F = {} C", -459.67, fahrenheit_to_celsius(20.0));
+            // println!("{} C = {} F", 0.0, celsius_to_fahrenheit(0.0));
     }
 }
