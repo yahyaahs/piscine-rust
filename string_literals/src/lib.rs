6de47b2d8ae1ce7e135dba5828e63cfce47965dc
yahya houssaini <erensas007@gmail.com>
2025-06-13 17:26:20 +0100

    string literals

---
+++ string_literals/src/lib.rs
@@ -0,0 +1,32 @@
+pub fn is_empty(v: &str) -> bool {
+    v.is_empty()
+
+}
+
+pub fn is_ascii(v: &str) -> bool {
+    v.is_ascii()
+}
+
+pub fn contains(v: &str, pat: &str) -> bool {
+    v.contains(pat)
+}
+
+pub fn split_at(v: &str, index: usize) -> (&str, &str) {
+    v.split_at(index)
+
+}
+
+pub fn find(v: &str, pat: char) -> usize {
+    v.find(pat).unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
