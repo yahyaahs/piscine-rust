55823e44e72a3619fe9b47c0a9522fa7c58bee7d
yahya houssaini <erensas007@gmail.com>
2025-06-13 17:32:12 +0100

    name initials harry potter

---
+++ name_initials/src/lib.rs
@@ -0,0 +1,25 @@
+pub fn initials(names: Vec<&str>) -> Vec<String> {
+    let mut new = Vec::new();
+    let mut initials = String::new();
+
+    for words in names{
+        for name in words.split(" "){
+            initials+= &(String::from(name.chars().nth(0).unwrap())+". ");
+        }
+        new.push(String::from(initials.trim()));
+        initials =String::new();
+
+    }
+    return new;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+    let names = vec!["Harry Potter", "Someone Else", "J. L.", "Barack Obama"];
+    println!("{:?}", initials(names));
+    }
+}
