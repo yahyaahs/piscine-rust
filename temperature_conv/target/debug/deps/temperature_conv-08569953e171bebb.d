a694a758d9e0689aaf04fb1de3846bc2b9ca6b90
yahya houssaini <erensas007@gmail.com>
2025-06-13 14:01:39 +0100

    targ

--- temperature_conv/target/debug/deps/temperature_conv-08569953e171bebb.d
+++
@@ -1,5 +0,0 @@
-/home/yhoussai/Desktop/piscine-rust/temperature_conv/target/debug/deps/temperature_conv-08569953e171bebb: src/lib.rs
-
-/home/yhoussai/Desktop/piscine-rust/temperature_conv/target/debug/deps/temperature_conv-08569953e171bebb.d: src/lib.rs
-
-src/lib.rs:
