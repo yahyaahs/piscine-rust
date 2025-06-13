c8f18553d9c873309c541213244ac8e1bc130bc3
yahya houssaini <erensas007@gmail.com>
2025-06-13 14:49:22 +0100

    looping

---
+++ looping/src/main.rs
@@ -0,0 +1,16 @@
+ use std::io;
+fn main() {
+    let stdin = io::stdin();
+    let input = &mut String::new();
+    let output = "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?";
+    let mut i = 1;
+    loop {
+        println!("{}", output);
+        let _ = stdin.read_line(input);
+        if input == "The letter e\n" {
+            println!("Number of trials: {}", i);
+            break;
+        }
+         i+=1;
+    }
+}
