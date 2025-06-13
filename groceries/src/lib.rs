c6b44dc4cc256606e6cb2b28436bfa3576c032c9
yahya houssaini <erensas007@gmail.com>
2025-06-13 14:56:21 +0100

    groceries

---
+++ groceries/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn insert(vec: &mut Vec<String>, val: String) {
+    vec.push(val)
+
+}
+
+pub fn at_index(slice: &[String], index: usize) -> &str {
+    &slice[index]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
