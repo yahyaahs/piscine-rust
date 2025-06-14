dfe6b7af1187e103e5c384c05bd7acfeef9e0274
yahya houssaini <erensas007@gmail.com>
2025-06-14 19:25:22 +0100

    arrays

---
+++ arrays/src/lib.rs
@@ -0,0 +1,26 @@
+pub fn sum(a: &[i32]) -> i32 {
+    let mut sum = 0;
+    for n in nums {
+        sum+= n;
+    }
+    sum
+}
+
+pub fn thirtytwo_tens() -> [i32; 32] {
+    let mut arr = [0; 32];
+    for i in 0..32 {
+        arr[i]= 10
+    }
+    arr
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
