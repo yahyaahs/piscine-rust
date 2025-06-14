d67c4416663fa42d16b6b25b9abd026c39d62e90
yahya houssaini <erensas007@gmail.com>
2025-06-14 16:56:25 +0100

    copy

--- copy/src/lib.rs
+++ copy/src/lib.rs
@@ -1,14 +1,34 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn nbr_function(c: i32) -> (i32, f64, f64) {
+    (c, (c as f64).exp(), (c as f64).abs().ln())
 }
 
+pub fn str_function(a: String) -> (String, String) {
+    let mut new_str = String::new();
+    for c in a.split_whitespace(){
+        new_str+= &(((c.parse::<f64>().unwrap()).exp()).to_string()+" ");
+    }
+    (a, (new_str.trim()).to_string())
+}
+
+pub fn vec_function(b: Vec<i32>) -> (Vec<i32>, Vec<f64>) {
+    let mut new_vec= vec![];
+    for nums in b.iter(){
+        new_vec.push((*nums as f64).abs().ln());
+    }
+    return (b, new_vec);
+}
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    let a = "1 2 4 5 6".to_owned();
+    let b = vec![1, 2, 4, 5];
+    let c = 0;
+
+    println!("{:?}", nbr_function(c));
+    println!("{:?}", vec_function(b));
+    println!("{:?}", str_function(a));
     }
 }
