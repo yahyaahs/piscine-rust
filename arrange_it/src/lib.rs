98d9efc76196f640e8211971b6d67c7858738102
yahya houssaini <erensas007@gmail.com>
2025-06-14 18:08:08 +0100

    arrange it

---
+++ arrange_it/src/lib.rs
@@ -0,0 +1,24 @@
+pub fn arrange_phrase(phrase: &str) -> String {
+    let mut i = 1;
+    let mut new:Vec<String>= vec![];
+    while new.len() < phrase.split_whitespace().collect::<Vec<_>>().len(){
+        for words in phrase.split_whitespace(){
+            if words.contains(&i.to_string()){
+                new.push(words.replace(&i.to_string(), ""));
+                i+=1;
+            }
+        }
+
+    }
+    new.join(" ")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+                println!("the phrase {}", arrange_phrase("is2 Thi1s T4est 3a"));
+    }
+}
