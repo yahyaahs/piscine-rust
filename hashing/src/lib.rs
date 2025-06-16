bd0ae5d18cd77160f0bf86168a31890be5742e3a
yahya houssaini <erensas007@gmail.com>
2025-06-16 16:23:50 +0100

    the median need /2

--- hashing/src/lib.rs
+++ hashing/src/lib.rs
@@ -8,7 +8,7 @@ pub fn median(list: &[i32]) -> i32 {
     let mut sorted = list.to_vec();
     sorted.sort();
     if sorted.len()%2==0{
-        sorted[sorted.len()/2-1] + sorted[sorted.len()/2]
+        (sorted[sorted.len()/2-1] + sorted[sorted.len()/2])/2
     }else{
         sorted[sorted.len()/2]
     }
