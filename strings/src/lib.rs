d597866f89cc20c7e1c9e8957e0ed5db322495dc
yahya houssaini <erensas007@gmail.com>
2025-06-16 14:06:53 +0100

    strings

---
+++ strings/src/lib.rs
@@ -0,0 +1,16 @@
+pub fn char_length(s: &str) -> usize {
+    s.chars().count()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+    println!("length of {} = {}", "❤", char_length("❤"));
+	println!("length of {} = {}", "形声字", char_length("形聲字"));
+	println!("length of {} = {}", "change", char_length("change"));
+	println!("length of {} = {}", "😍", char_length("😍"));
+    }
+}
