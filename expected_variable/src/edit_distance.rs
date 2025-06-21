af702ccdc1dbc0f6593a9d560beca63728801884
yahya houssaini <erensas007@gmail.com>
2025-06-21 17:14:16 +0100

    hqsdklfqsdf

---
+++ expected_variable/src/edit_distance.rs
@@ -0,0 +1,24 @@
+pub fn edit_distance(source: &str, target: &str) -> usize {
+    let l1 = source.len();
+    let l2 = target.len();
+    let source_chars: Vec<char> = source.chars().collect();
+    let target_chars: Vec<char> = target.chars().collect();
+
+    let mut matrix = vec![vec![0; l2 + 1]; l2 + 1];
+
+    for i in 0..=l1 {
+        for j in 0..=l2 {
+            matrix[i][j] = if i == 0 {
+                j
+            } else if j == 0 {
+                i
+            } else if source_chars[i - 1] == target_chars[j - 1] {
+                matrix[i - 1][j - 1]
+            } else {
+                1 + matrix[i - 1][j - 1].min(matrix[i][j - 1]).min(matrix[i - 1][j])
+            };
+        }
+    }
+
+    matrix[l1][l2]
+}
---
+++ expected_variable/src/lib.rs
@@ -0,0 +1,37 @@
+pub mod edit_distance;
+use std::fmt::format;
+
+use edit_distance::*;
+pub fn expected_variable(first : &str, sec :&str)->Option<String>{
+    let steps = edit_distance(&first.to_lowercase(), &sec.to_lowercase());
+    let calc = (sec.len()- steps) as f64/sec.len() as f64*100.;
+    if calc  < 50. {
+        return None;
+    }
+    return Some(format!("{}%", calc.round().to_string()));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+            println!(
+        "{} close to it",
+        expected_variable("On_Point", "on_point").unwrap()
+    );
+    println!(
+        "{} close to it",
+        expected_variable("soClose", "so_close").unwrap()
+    );
+    println!(
+        "{:?}",
+        expected_variable("something", "something_completely_different")
+    );
+    println!(
+        "{} close to it",
+        expected_variable("BenedictCumberbatch", "BeneficialCucumbersnatch").unwrap()
+    );
+    }
+}
