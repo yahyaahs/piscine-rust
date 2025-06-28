f4a53d96bc258103aac774870db5b10ad57d9270
yahya houssaini <erensas007@gmail.com>
2025-06-28 15:59:28 +0100

    no string

--- how_many_references/src/lib.rs
+++ how_many_references/src/lib.rs
@@ -13,7 +13,7 @@ impl Node {
         self.ref_list.push(element);
     }
     pub fn rm_all_ref(&mut self, element: Rc<String>) {
-        self.ref_list.retain(|el| !Rc::ptr_eq(el, &element)&& element!=*el );
+        self.ref_list.retain(|el| !Rc::ptr_eq(el, &element));
     }
 }
 
@@ -50,5 +50,46 @@ mod tests {
     println!("a: {:?}", how_many_references(&a));
     println!("b: {:?}", how_many_references(&b));
     println!("c: {:?}", how_many_references(&c));
+    }
+        #[test]
+    fn test_rm_all_ref() {
+        let a = Rc::new(String::from("a"));
+        let b = Rc::new(String::from("b"));
+        let c = Rc::new(String::from("c"));
+        let d = Rc::new(String::from("d"));
+
+        let a1 = Rc::new(String::from("a"));
+        let b1 = Rc::new(String::from("b"));
+        let c1 = Rc::new(String::from("c"));
+        let d1 = Rc::new(String::from("d"));
+        let mut new_node = Node::new(vec![
+            d.clone(),
+            d.clone(),
+            b.clone(),
+            a.clone(),
+            c.clone(),
+            a.clone(),
+            d.clone(),
+        ]);
+
+        new_node.rm_all_ref(a1.clone());
+        assert_eq!(how_many_references(&a), 3);
+        new_node.rm_all_ref(a.clone());
+        assert_eq!(how_many_references(&a), 1);
+
+        new_node.rm_all_ref(b1.clone());
+        assert_eq!(how_many_references(&b), 2);
+        new_node.rm_all_ref(b.clone());
+        assert_eq!(how_many_references(&b), 1);
+
+        new_node.rm_all_ref(c1.clone());
+        assert_eq!(how_many_references(&c), 2);
+        new_node.rm_all_ref(c.clone());
+        assert_eq!(how_many_references(&c), 1);
+
+        new_node.rm_all_ref(d1.clone());
+        assert_eq!(how_many_references(&d), 4);
+        new_node.rm_all_ref(d.clone());
+        assert_eq!(how_many_references(&d), 1);
     }
 }
