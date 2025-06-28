545dec931c81079b8586754508505be6a8f0c73f
yahya houssaini <erensas007@gmail.com>
2025-06-28 20:22:12 +0100

    formatttttttttttttttting

--- ref_cell/src/lib.rs
+++ ref_cell/src/lib.rs
@@ -1,8 +1,8 @@
 mod messenger;
-use std::collections::HashMap;
 pub use messenger::*;
-pub use std::rc::Rc;
 pub use std::cell::RefCell;
+use std::collections::HashMap;
+pub use std::rc::Rc;
 
 #[derive(Debug)]
 pub struct Worker {
@@ -18,20 +18,25 @@ impl Worker {
             all_messages: RefCell::new(Vec::new()),
         }
     }
-
 }
 impl Logger for Worker {
     fn error(&self, msg: &str) {
-        self.all_messages.borrow_mut().push(format!("Error: {}", msg));
-        self.mapped_messages.borrow_mut().insert("Error".to_string(), msg.to_string());
+        self.all_messages.borrow_mut().push(msg.to_string());
+        self.mapped_messages
+            .borrow_mut()
+            .insert("Error".to_string(), msg.replace("Error: ", ""));
     }
     fn info(&self, msg: &str) {
-        self.all_messages.borrow_mut().push(format!("Info: {}", msg));
-        self.mapped_messages.borrow_mut().insert("Info".to_string(), msg.to_string());
+        self.all_messages.borrow_mut().push(msg.to_string());
+        self.mapped_messages
+            .borrow_mut()
+            .insert("Info".to_string(), msg.replace("Info: ", ""));
     }
     fn warning(&self, msg: &str) {
-        self.all_messages.borrow_mut().push(format!("Warning: {}", msg));
-        self.mapped_messages.borrow_mut().insert("Warning".to_string(), msg.to_string());
+        self.all_messages.borrow_mut().push(msg.to_string());
+        self.mapped_messages
+            .borrow_mut()
+            .insert("Warning".to_string(), msg.replace("Warning: ", ""));
     }
 }
 
@@ -42,43 +47,42 @@ mod tests {
 
     #[test]
     fn it_works() {
-     // initialize the worker
-    let logger = Worker::new(1);
+        // initialize the worker
+        let logger = Worker::new(1);
 
-    // initialize the tracker, with the max number of
-    // called references as 10
-    let track = Tracker::new(&logger, 10);
+        // initialize the tracker, with the max number of
+        // called references as 10
+        let track = Tracker::new(&logger, 10);
 
-    let _a = logger.track_value.clone();    // |\
-    let _a1 = logger.track_value.clone();   // | -> increase the Rc to 4 references
-    let _a2 = logger.track_value.clone();   // |/
+        let _a = logger.track_value.clone(); // |\
+        let _a1 = logger.track_value.clone(); // | -> increase the Rc to 4 references
+        let _a2 = logger.track_value.clone(); // |/
 
-    // take a peek of how much we already used from our quota
-    track.peek(&logger.track_value);
+        // take a peek of how much we already used from our quota
+        track.peek(&logger.track_value);
 
-    let _b = logger.track_value.clone();  // |\
-    let _b1 = logger.track_value.clone(); // |  -> increase the Rc to 8 references
-    let _b2 = logger.track_value.clone(); // | /
-    let _b3 = logger.track_value.clone(); // |/
+        let _b = logger.track_value.clone(); // |\
+        let _b1 = logger.track_value.clone(); // |  -> increase the Rc to 8 references
+        let _b2 = logger.track_value.clone(); // | /
+        let _b3 = logger.track_value.clone(); // |/
 
-    // this will set the value and making a verification of
-    // how much we already used of our quota
-    track.set_value(&logger.track_value);
+        // this will set the value and making a verification of
+        // how much we already used of our quota
+        track.set_value(&logger.track_value);
 
-    let _c = logger.track_value.clone(); // | -> increase the Rc to 9 references
+        let _c = logger.track_value.clone(); // | -> increase the Rc to 9 references
 
-    // this will set the value and making a verification of
-    // how much we already used of our quota
-    track.set_value(&logger.track_value);
+        // this will set the value and making a verification of
+        // how much we already used of our quota
+        track.set_value(&logger.track_value);
 
-    let _c1 = logger.track_value.clone(); // | -> increase the Rc to 10 references, this will be the limit
+        let _c1 = logger.track_value.clone(); // | -> increase the Rc to 10 references, this will be the limit
 
-    track.set_value(&logger.track_value);
+        track.set_value(&logger.track_value);
 
-    for (k ,v) in logger.mapped_messages.into_inner() {
-        println!("{:?}", (k ,v));
+        for (k, v) in logger.mapped_messages.into_inner() {
+            println!("{:?}", (k, v));
+        }
+        println!("{:?}", logger.all_messages.into_inner());
     }
-    println!("{:?}", logger.all_messages.into_inner());
-    
-}
 }
--- ref_cell/src/messenger.rs
+++ ref_cell/src/messenger.rs
@@ -26,10 +26,10 @@ impl<'a> Tracker<'a> {
         let percentage = (count as f64 / self.max as f64) * 100.0;
         let percent_int = percentage as u32;
         if percentage >= 100.0 {
-            self.logger.error("you are over your quota!");
+            self.logger.error("Error: you are over your quota!");
         } else if percentage >= 70.0 {
             self.logger.warning(&format!(
-                "you have used up over {}% of your quota! Proceeds with precaution",
+                "Warning: you have used up over {}% of your quota! Proceeds with precaution",
                 percent_int
             ));
         }
@@ -40,6 +40,6 @@ impl<'a> Tracker<'a> {
         let percentage = (count as f64 / self.max as f64) * 100.0;
         let percent_int = percentage as u32;
         self.logger
-            .info(&format!("you are using up to {}% of your quota", percent_int));
+            .info(&format!("Info: you are using up to {}% of your quota", percent_int));
     }
 }
