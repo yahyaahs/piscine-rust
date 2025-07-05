8abc0649335e8d91874484ef4d2b271c3af7a111
yahya houssaini <erensas007@gmail.com>
2025-07-05 20:06:45 +0100

    closures

---
+++ closures/src/lib.rs
@@ -0,0 +1,25 @@
+
+fn first_fifty_even_square() -> Vec<i32> {
+
+    let is_even = |n : i32| n % 2 == 0;
+    let mut  v = Vec::new();
+    let mut count = 1;
+    while v.len() <50{
+        if is_even(count){
+            v.push(count *count);
+        }
+        count+=1;
+    }
+    v
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+	let v1 = first_fifty_even_square();
+
+	println!("All elements in {:?}, len = {}", v1, v1.len());
+    }
+}
