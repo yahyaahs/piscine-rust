69bdd2b5f0a679ee3457437a97f7dfa07942989d
yahya houssaini <erensas007@gmail.com>
2025-07-07 18:47:47 +0100

    products

---
+++ get_products/src/lib.rs
@@ -0,0 +1,29 @@
+pub fn get_products(arr: Vec<usize>) -> Vec<usize> {
+    let mut sum = 1;
+    let mut count = 0;
+    let mut v = Vec::new();
+    while arr.len() > v.len() {
+        for (i, el) in arr.iter().enumerate() {
+            if count == i {
+                continue;
+            }
+            sum *= el;
+        }
+        v.push(sum);
+        sum = 1;
+                count += 1
+    }
+
+    v
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let arr: Vec<usize> = vec![1, 7, 3, 4];
+        let output = get_products(arr);
+        println!("{:?}", output);
+    }
+}
