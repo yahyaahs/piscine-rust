678749f445f3205e073853e4d198d4bf37952004
yahya houssaini <erensas007@gmail.com>
2025-07-07 17:24:20 +0100

    sort

--- sales/src/lib.rs
+++ sales/src/lib.rs
@@ -1,4 +1,3 @@
-
 #[derive(Debug, Clone, PartialEq)]
 pub struct Store {
     pub products: Vec<(String, f32)>,
@@ -12,32 +11,32 @@ impl Store {
 #[derive(Debug, Clone, PartialEq)]
 pub struct Cart {
     // expected public fields
-    pub buy : Vec<(String, f32)>,
-    pub receipt : Vec<f32>,
+    pub buy: Vec<(String, f32)>,
+    pub receipt: Vec<f32>,
 }
 impl Cart {
     pub fn new() -> Cart {
-        Cart{
-            buy : Vec::new(),
-            receipt : Vec::new(),
+        Cart {
+            buy: Vec::new(),
+            receipt: Vec::new(),
         }
     }
     pub fn insert_item(&mut self, s: &Store, ele: String) {
-        let find_price = |s : &Store| s.products.iter().find(|x| x.0==ele).map(|p| p.1).unwrap();
+        let find_price = |s: &Store| s.products.iter().find(|x| x.0 == ele).map(|p| p.1).unwrap();
         self.buy.push((ele.clone(), find_price(s)));
         self.receipt.push(find_price(s));
     }
     pub fn generate_receipt(&mut self) -> Vec<f32> {
-
-    self.buy.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
-    let count = self.buy.len()/3;
-        let find_cheapest = |v : &Vec<f32>|  self.buy.iter().take(count).map(|x| x.1).sum::<f32>();
-        let base = find_cheapest(&self.receipt);
-        let total:f32 = self.buy.iter().map(|x| x.1).sum();
-  let reduction = |price: f32| {
-        let discounted = price - (price / total * base);
-        (discounted * 100.0).round() / 100.0 
-    };        let rec: Vec<f32> = self.receipt.iter().map(|&el | reduction(el)).collect();
+        &self.buy.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
+        let count = self.buy.len() / 3;
+        let find_cheapest = |v: &Vec<(String, f32)>| v.iter().take(count).map(|x| x.1).sum::<f32>();
+        let base = find_cheapest(&self.buy);
+        let total: f32 = self.buy.iter().map(|x| x.1).sum();
+        let reduction = |price: f32| {
+            let discounted = price - (price / total * base);
+            (discounted * 100.0).round() / 100.0
+        };
+        let rec: Vec<f32> = self.receipt.iter().map(|&el| reduction(el)).collect();
         return rec;
     }
 }
