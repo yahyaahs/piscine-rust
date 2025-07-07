8202f0f26d77fec57cce5875e07c580b1bfd6808
yahya houssaini <erensas007@gmail.com>
2025-07-07 19:31:02 +0100

    1_2 iter

--- iterators/src/lib.rs
+++ iterators/src/lib.rs
@@ -4,30 +4,27 @@ pub struct Collatz {
 }
 
 impl Iterator for Collatz {
-    type Item = u64;
+    type Item = Collatz;
 
-fn next(&mut self) -> Option<Self::Item> {
-    if self.v == 0 {
-        return None;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.v == 0 || self.v == 1 {
+            return None;
+        }
+
+        let current = *self;
+
+        if self.v % 2 == 0 {
+            self.v /= 2;
+        } else {
+            self.v = 3 * self.v + 1;
+        }
+
+        Some(current)
     }
-
-    let current = self.v;
-
-    if self.v == 1 {
-        self.v = 0; 
-    } else if self.v % 2 == 0 {
-        self.v /= 2;
-    } else {
-        self.v = 3 * self.v + 1;
-    }
-
-    return Some(current);
-}
-
 }
 
 impl Collatz {
-	pub fn new(n: u64) -> Self {
+    pub fn new(n: u64) -> Self {
         Self { v: n }
     }
 }
@@ -37,7 +34,7 @@ pub fn collatz(n: u64) -> usize {
     let mut count = 0;
 
     while let Some(x) = collatz_iter.next() {
-        if x == 1 {
+        if x.v == 1 {
             break;
         }
         count += 1;
@@ -46,19 +43,18 @@ pub fn collatz(n: u64) -> usize {
     count
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-            println!("{:?}", collatz(0));
-    println!("{:?}", collatz(1));
-    println!("{:?}", collatz(4));
-    println!("{:?}", collatz(5));
-    println!("{:?}", collatz(6));
-    println!("{:?}", collatz(7));
-    println!("{:?}", collatz(12));
+    fn test_collatz() {
+        println!("{:?}", collatz(0));
+        println!("{:?}", collatz(1));
+        println!("{:?}", collatz(4));
+        println!("{:?}", collatz(5));
+        println!("{:?}", collatz(6));
+        println!("{:?}", collatz(7));
+        println!("{:?}", collatz(12));
     }
 }
